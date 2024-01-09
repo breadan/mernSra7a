239dcff1fe9a6605959bc4a7e82db8b33498c0ab
UrCodeBuddy <59134280+xUrCodeBuddyx@users.noreply.github.com>
2024-01-09 17:46:34 +0100

    Refactor message routes and imports

--- src/modules/message/message.routes.js
+++ src/modules/message/message.routes.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import { addMessage, getUserMessages, updateMessage } from '../../modules/message/messag.controller.js';
+import express from "express";
+import {
+  addMessage,
+  getUserMessages,
+  updateMessage,
+} from "../../modules/message/messag.controller.js";
 import validate from "../../middleware/validate.message.js";
-import { auth } from '../../middleware/auth.js';
-import verifyToken from '../../middleware/verifyToken.js';
+import { auth } from "../../middleware/auth.js";
+import { verifyToken } from "../../middleware/verifyToken.js";
 
 const messageRouter = express.Router();
 
-messageRouter.post('/', [validate.message,auth], addMessage);
-messageRouter.get('/', auth, getUserMessages);
-messageRouter.patch('/:id', [auth,verifyToken], updateMessage);
+messageRouter.post("/", [validate.message, auth], addMessage);
+messageRouter.get("/", auth, getUserMessages);
+messageRouter.patch("/:id", [auth, verifyToken], updateMessage);
 
 export default messageRouter;
