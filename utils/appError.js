ff07eda00403e1bb253ba51029f7f31a8fa71665
UrCodeBuddy <59134280+xUrCodeBuddyx@users.noreply.github.com>
2024-01-09 17:30:08 +0100

    Refactor error handling and add error logging

--- utils/appError.js
+++ utils/appError.js
@@ -1,12 +1,29 @@
-
-
 // handle error in project (2)
 
- export class AppError extends Error {
-    constructor (message, statusCode) {
-        super(message);
-        this.statusCode = statusCode 
+export class AppError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
+export const errorHandler = (err, res) => {
+  let { statusCode, message } = err;
 
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...{ stack: err.stack },
+  };
+
+  logger.error(err);
+
+  res.status(statusCode).send(response);
+};
