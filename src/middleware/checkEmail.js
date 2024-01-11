fb8160c3338335a6049b8e4a446b5c6a16ba29c4
UrCodeBuddy <59134280+xUrCodeBuddyx@users.noreply.github.com>
2024-01-11 19:48:24 +0100

    Refactor checkEmail middleware to use async/await syntax and update status code for existing email

--- src/middleware/checkEmail.js
+++ src/middleware/checkEmail.js
@@ -3,7 +3,7 @@ import httpStatusText from "../../utils/httpStatusText.js";
 import { AppError } from "../../utils/appError.js";
 import asyncError from "../../utils/asyncError.js";
 
-const middSignUp =asyncError( async (req, res, next) => {
+const middSignUp = asyncError(async (req, res, next) => {
   const user = await userModel.findOne({ email: req.body.email });
 
   if (user) {
@@ -11,7 +11,8 @@ const middSignUp =asyncError( async (req, res, next) => {
     //   status: httpStatusText.ERROR,
     //   message: "Email Already Exists",
     // });
-    next(new AppError(httpStatusText.ERROR, "Email Already Exists", 400));
+    // changed status code to 409
+    next(new AppError(409, "Email Already Exists"));
   }
   next();
 });
